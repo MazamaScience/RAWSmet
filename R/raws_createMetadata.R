c447aaee8ed529ad42943fc49a074c85cbbe970d
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2020-08-03 17:13:07 -0700

    refactor of raws_createMetadata.R

--- R/raws_createMetadata.R
+++ R/raws_createMetadata.R
@@ -5,6 +5,8 @@
 #' @title Obtain metadata for each station in a state.
 #'
 #' @param stateCode Two character state code (will be downcased).
+#' @param baseUrl Base URL for data queries.
+#' @param verbose Logical flag controlling detailed progress statements.
 #'
 #' @return Dataframe of station metadata.
 #'
@@ -28,7 +30,9 @@
 #' @references \href{https://raws.dri.edu/}{RAWS USA Climate Archive}
 
 raws_createMetadata <- function(
-  stateCode = NULL
+  stateCode = NULL,
+  baseUrl = "https://raws.dri.edu/",
+  verbose = FALSE
 ) {
 
   # ----- Validate parameters --------------------------------------------------
@@ -43,136 +47,107 @@ raws_createMetadata <- function(
   
   # NOTE: California is separated into north and south. We have to get these
   #       two lists separately. 
-  if( stateCode == 'ca' ) {
+  if ( stateCode == 'ca' ) {
     
-    northUrl <- paste0("https://raws.dri.edu/ncalst.html")
-    southUrl <- paste0("https://raws.dri.edu/scalst.html")
+    northUrl <- paste0(baseUrl, "ncalst.html")
+    southUrl <- paste0(baseUrl, "scalst.html")
     
-    monitorIDs <- 
+    stationIDs <- 
       MazamaCoreUtils::html_getLinkUrls(northUrl) %>%        # get links 
       append(MazamaCoreUtils::html_getLinkUrls(southUrl)) %>%
       stringr::str_subset("rawMAIN.pl\\?") %>%              # only keep those with "rawMAIN.pl?"
       stringr::str_match(".+MAIN.pl\\?(.+)") %>%            # pull out everything after "MAIN.pl?"
       magrittr::extract(, 2)                                # keep the second column of the matrix
+    
   } else {
     
-    url <- paste0("https://raws.dri.edu/", tolower(stateCode), "lst.html")
+    url <- paste0(baseUrl, tolower(stateCode), "lst.html")
     
-    monitorIDs <- 
+    stationIDs <- 
       MazamaCoreUtils::html_getLinkUrls(url) %>%  # get links 
       stringr::str_subset("rawMAIN.pl\\?") %>%    # only keep those with "rawMAIN.pl?"
       stringr::str_match(".+MAIN.pl\\?(.+)") %>%  # pull out everything after "MAIN.pl?"
-      magrittr::extract(, 2)                      # keep the second column of the matrix    
+      magrittr::extract(, 2)                      # keep the second column of the matrix   
+    
   }
   
-  # ----- Get station metadata ------------------------------------------------------
+  # Filter for stations not in the requested state
+  stationIDs <- stringr::str_subset(stationIDs, paste0("^", stateCode) )
   
-  metadataList <- lapply(monitorIDs, raws_getStationMetadata)
-  metadata <- dplyr::bind_rows(metadataList)
+  # ----- Loop over stationIDs -------------------------------------------------
   
-  # Filter off stations not in the given state
-  givenStateCode <- stateCode
-  metadata <- metadata %>% dplyr::filter(metadata$stateCode == toupper(givenStateCode))
-  return(metadata)
-}
+  recordList <- list()
+  i <- 0
+  for ( stationID in stationIDs ) {
+   
+    i <- i + 1
+    if ( verbose )
+      message(sprintf("Working on %03d/%03d: %s...", i, length(stationIDs), stationID))
+      
+    # * Get the data -----
+    
+    metaUrl <- paste0("https://raws.dri.edu/cgi-bin/wea_info.pl?", stationID)
+    
+    tables <- MazamaCoreUtils::html_getTables(metaUrl)
+    
+    # NOTE: If a station has photos, the first table on the site will contain the photos.
+    if ( length(tables) > 1) {
+      metaTable <- tables[[2]]
+    } else {
+      metaTable <- tables[[1]]
+    }
+    
+    siteName <- metaTable$X2[metaTable$X1 == "Location"]
+    latitudeDMS <- metaTable$X2[metaTable$X1 == "Latitude"]
+    longitudeDMS <- metaTable$X2[metaTable$X1 == "Longitude"]
+    elevation <- metaTable$X2[metaTable$X1 == "Elevation"]
+    
+    # * Convert to internal standard -----
+    
+    # Scrape the coordinate data
+    # NOTE: Coordinate data comes in like "48° 44' 35&quot"
+    latitudeSplit <- stringr::str_split(latitudeDMS, " ", simplify = TRUE)
+    longitudeSplit <- stringr::str_split(longitudeDMS, " ", simplify = TRUE)
+    
+    latitudeDegrees <- as.integer(stringr::str_sub(latitudeSplit[1], end = -2))
+    latitudeMinutes <- as.integer(stringr::str_sub(latitudeSplit[2], end = -2))
+    latitudeSeconds <- as.integer(stringr::str_sub(latitudeSplit[3], end = -6))
+    
+    longitudeDegrees <- as.integer(stringr::str_sub(longitudeSplit[1], end = -2))
+    longitudeMinutes <- as.integer(stringr::str_sub(longitudeSplit[2], end = -2))
+    longitudeSeconds <- as.integer(stringr::str_sub(longitudeSplit[3], end = -6))
+    
+    # Covert degrees/minutes/seconds coordinates to decimal degrees
+    latitude <- latitudeDegrees + latitudeMinutes/60 + latitudeSeconds/3600
+    # NOTE: The longitude is in degrees west. Negate to get it in degrees east.
+    longitude <- -(longitudeDegrees + longitudeMinutes/60 + longitudeSeconds/3600)
+    
+    # Convert elevation in feet to meters
+    elevation <- as.integer(stringr::str_sub(elevation, end = -5)) * 0.3048
+    
+    # Remove extra whitespace in siteName
+    siteName <- gsub("\\s+", " ", siteName)
+    
+    recordList[[stationID]] <-
+      dplyr::tibble(
+        "countryCode" = "US", 
+        "stateCode" = toupper(stateCode), 
+        "stationID" = stationID, 
+        "siteName" = siteName, 
+        "longitude" = longitude, 
+        "latitude" = latitude, 
+        "elevation" = elevation      
+      )
+    
+  }
+  
+  # ----- Assemble dataframe ---------------------------------------------------
+  
+  meta <- dplyr::bind_rows(recordList)
+  
+  # Now add the timezeon
+  meta$timezone <- MazamaSpatialUtils::getTimezone(meta$longitude, meta$latitude)
 
-#' @rdname raws_createMetadata
-#' @param stationID Four character station ID or six character combination of state code and station ID (e.g. waWASH).
-#' @export
-raws_getStationMetadata <- function(
-  stationID = NULL,
-  stateCode = NULL
-) {
+  return(meta)
   
-  # ----- Setup ---------------------------------------------------------------
-  
-  loadSpatialData("USCensusStates")
-  
-  # ----- Validate parameters --------------------------------------------------
-  
-  stopIfNull(stationID)
-  
-  if (is.null(stateCode) && nchar(stationID) == 6) {
-    stateCode <- stringr::str_sub(stationID, 0, 2)
-    stationID <- stringr::str_sub(stationID, -4)
-  } else {
-    stopIfNull(stateCode)
-  }
-  
-  # TODO: Check if stateCode is in list of state codes.
-  
-  # ----- Get the data ---------------------------------------------------------
-  
-  metaUrl <- paste0("https://raws.dri.edu/cgi-bin/wea_info.pl?", tolower(stateCode), toupper(stationID))
-  
-  tables <- MazamaCoreUtils::html_getTables(metaUrl)
-  
-  # NOTE: If a station has photos, the first table on the site will contain the photos.
-  if( length(tables) > 1) {
-    metaTable <- tables[[2]]
-  } else {
-    metaTable <- tables[[1]]
-  }
-  
-  siteName <- metaTable$X2[metaTable$X1 == "Location"]
-  latitudeDMS <- metaTable$X2[metaTable$X1 == "Latitude"]
-  longitudeDMS <- metaTable$X2[metaTable$X1 == "Longitude"]
-  elevation <- metaTable$X2[metaTable$X1 == "Elevation"]
-  
-  # ----- Organize the data ----------------------------------------------------
-  
-  # Scrape the coordinate data
-  # NOTE: Coordinate data comes in like 48° 44' 35"
-  latitudeSplit <- stringr::str_split(latitudeDMS, " ", simplify = TRUE)
-  longitudeSplit <- stringr::str_split(longitudeDMS, " ", simplify = TRUE)
-  
-  latitudeDegrees <- as.integer(stringr::str_sub(latitudeSplit[1], end = -2))
-  latitudeMinutes <- as.integer(stringr::str_sub(latitudeSplit[2], end = -2))
-  latitudeSeconds <- as.integer(stringr::str_sub(latitudeSplit[3], end = -6))
-  
-  longitudeDegrees <- as.integer(stringr::str_sub(longitudeSplit[1], end = -2))
-  longitudeMinutes <- as.integer(stringr::str_sub(longitudeSplit[2], end = -2))
-  longitudeSeconds <- as.integer(stringr::str_sub(longitudeSplit[3], end = -6))
-  
-  # Covert degrees/minutes/seconds coordinates to decimal degrees
-  latitude <- latitudeDegrees + latitudeMinutes/60 + latitudeSeconds/3600
-  # NOTE: The longitude is in degrees west. Negate to get it in degrees east.
-  longitude <- -(longitudeDegrees + longitudeMinutes/60 + longitudeSeconds/3600)
-  
-  # Convert elevation in feet to meters
-  elevation <- as.integer(stringr::str_sub(elevation, end = -5)) * 0.3048
-  
-  # Remove extra whitespace in siteName
-  siteName <- gsub("\\s+", " ", siteName)
-  
-  # ----- Get extra information ------------------------------------------------
-  
-  # Get state code
-  stateCode <- MazamaSpatialUtils::getStateCode(
-    lon = longitude,
-    lat = latitude,
-    dataset = 'USCensusStates',
-    useBuffering = TRUE
-  )
-  
-  # Get country code
-  countryCode <- MazamaSpatialUtils::getCountryCode(
-    lon = longitude,
-    lat = latitude,
-    useBuffering = TRUE
-  )
-  
-  # Get timezone
-  timezone <- MazamaSpatialUtils::getTimezone(
-    lon = longitude,
-    lat = latitude,
-    useBuffering = TRUE
-  )
-  
-  # ----- Return ---------------------------------------------------------------
-  
-  ret <- list("stationID" = stationID, "siteName" = siteName, "longitude" = longitude, "latitude" = latitude, 
-              "elevation" = elevation, "countryCode" = countryCode, "stateCode" = stateCode, "timezone" = timezone)
-  
-  return(ret)
 }
