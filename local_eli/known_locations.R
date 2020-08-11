c8d5ca89b05f3e7e5ce6cb1d637fc026490ea134
Eli Grosman <eligrosman1@gmail.com>
2020-08-11 16:51:35 -0700

    local testing script

---
+++ local_eli/known_locations.R
@@ -0,0 +1,22 @@
+library(MazamaSpatialUtils)
+MazamaSpatialUtils::setSpatialDataDir("~/Data/Spatial")
+
+library(MazamaLocationUtils)
+MazamaLocationUtils::mazama_initialize()
+
+library(RAWSmet)
+RAWSmet::setRawsDataDir("~/Data/RAWS")
+rawsDataDir <- RAWSmet::getRawsDataDir()
+
+meta <- fw13_createMetadata()
+
+known_locations <- MazamaLocationUtils::table_initializeExisting(meta)
+
+known_locations$deviceDeploymentID <- 
+  sprintf(
+    "%s_%s",
+    known_locations$locationID,
+    known_locations$nwsID
+  )
+
+save(known_locations, file = paste0(rawsDataDir, '/known_locations.rda'))
