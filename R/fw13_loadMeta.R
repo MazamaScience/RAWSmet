6c6865270ec4a6de62151c3aa58b8dfd67879ac7
Eli Grosman <eligrosman1@gmail.com>
2020-08-27 16:06:16 -0700

    add fw13/wrcc_loadMeta functions

---
+++ R/fw13_loadMeta.R
@@ -0,0 +1,69 @@
+#' @export
+#' 
+#' @title Load FW13 RAWS station metadata from a local directory
+#'
+#' @param metadataUrl URL for station metadata.
+#' @param verbose Logical flag controlling detailed progress statements.
+#'
+#' @return Dataframe containing station metadata.
+#'
+#' @description Loads all FW13 station metadata from the \code{rawsDataDir}. If the
+#' data is not in this directory, this will download and save the data. 
+#' 
+#' @examples
+#' \dontrun{
+#' library(RAWSmet)
+#' 
+#' setRawsDataDir("~/Data/RAWS/")
+#'
+#' stationMeta <- fw13_loadMeta()
+#' 
+#' dplyr::glimpse(stationMeta)
+#' }
+#'
+#' @seealso \code{fw13_createMetadata}
+#' @references \href{https://cefa.dri.edu/raws/}{Program for Climate, Ecosystem and Fire Applications}
+
+fw13_loadMeta <- function(
+  metadataUrl = "https://cefa.dri.edu/raws/RAWSfw13list.xlsx",
+  verbose = TRUE
+) {
+  
+  # ----- Setup ----------------------------------------------------------------
+  
+  dataDir <- getRawsDataDir()
+  
+  # ----- Check for local data -------------------------------------------------
+  
+  fileName = "fw13Metadata.rda"
+  filePath = file.path(dataDir, fileName)
+  
+  if ( file.exists(filePath) ) {
+    
+    if( verbose ) {
+      message("Loading data from local directory.")
+    }
+    
+    # If local data exists, load and return it.
+    metadata <- get(load(filePath))
+    
+  } else {
+    
+    if ( verbose ) {
+      message("Could not find local data.")
+      message(paste("Downloading and saving data to", filePath, "."))
+    }
+    
+    # If local data does not exist, download and return it.
+    metadata <- fw13_createMetadata(metadataUrl = metadataUrl, verbose = verbose)
+  
+    # Save this object so it may be loaded in the future
+    save(metadata, file = filePath)
+    
+  }
+  
+  # ----- Return ---------------------------------------------------------------
+  
+  return(metadata)
+  
+}
---
+++ R/wrcc_loadMeta.R
@@ -0,0 +1,73 @@
+#' @export
+#' 
+#' @title Load WRCC RAWS station metadata from a local directory
+#'
+#' @param stateCode Two character state code (will be downcased).
+#' @param baseUrl Base URL for data queries.
+#' @param verbose Logical flag controlling detailed progress statements.
+#'
+#' @return Dataframe containing station metadata.
+#'
+#' @description Loads WRCC station metadata for a given state from the \code{rawsDataDir}. If the
+#' data is not in this directory, this will download and save the data. 
+#' 
+#' @examples
+#' \dontrun{
+#' library(RAWSmet)
+#' 
+#' setRawsDataDir("~/Data/RAWS/")
+#'
+#' stationMeta <- wrcc_loadMeta(stateCode = "WA")
+#' 
+#' dplyr::glimpse(stationMeta)
+#' }
+#'
+#' @seealso \code{wrcc_createMetadata}
+#' @references \href{https://cefa.dri.edu/raws/}{Program for Climate, Ecosystem and Fire Applications}
+
+wrcc_loadMeta <- function(
+  stateCode = NULL,
+  baseUrl = "https://raws.dri.edu/",
+  verbose = TRUE
+) {
+  
+  # ----- Setup ----------------------------------------------------------------
+  
+  MazamaCoreUtils::stopIfNull(stateCode)
+  
+  dataDir <- getRawsDataDir()
+  
+  # ----- Check for local data -------------------------------------------------
+  
+  fileName = sprintf("wrccMetadata_%s.rda", stateCode)
+  filePath = file.path(dataDir, fileName)
+  
+  if ( file.exists(filePath) ) {
+    
+    if( verbose ) {
+      message("Loading data from local directory.")
+    }
+    
+    # If local data exists, load and return it.
+    metadata <- get(load(filePath))
+    
+  } else {
+    
+    if ( verbose ) {
+      message("Could not find local data.")
+      message(paste("Downloading and saving data to", filePath, "."))
+    }
+    
+    # If local data does not exist, download and return it.
+    metadata <- wrcc_createMetadata(stateCode = stateCode, baseUrl = baseUrl, verbose = verbose)
+    
+    # Save this object so it may be loaded in the future
+    save(metadata, file = filePath)
+    
+  }
+  
+  # ----- Return ---------------------------------------------------------------
+  
+  return(metadata)
+  
+}
