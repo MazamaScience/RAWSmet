39b665027c90af048b5891b6fd33a0d23cbd86f1
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2020-08-31 13:51:04 -0700

    more comments in example_timeseries.R

--- local_jon/example_timeseries.R
+++ local_jon/example_timeseries.R
@@ -1,26 +1,38 @@
 # Example timeseries plots
 
+# Load required MazamaSpatialUtils
 library(MazamaSpatialUtils)
+
+# Set directory of previously installed spatial data
 setSpatialDataDir("~/Data/Spatial")
 
+# Load RAWSmet
 library(RAWSmet)
+
+# Set directory where RAWS data will be stored for reuse
 setRawsDataDir("~/Data/RAWS")
 
+# Download/reload metadata from the "FW13" site
 meta <- fw13_loadMeta(verbose = TRUE)
 
+# See what stations are available
 raws_leaflet(meta)
 
+# Use a stationID to download historical data
 bend_1 <- fw13_load("352621", meta)
 
+# "raws" objects are a list with 'meta' and 'data'
+
+# Review the time range
 range(bend_1$data$datetime)
 
+# Use "recipe style" pipeline to extract data for use with the *openair* package
 data_2017 <- 
   bend_1 %>% 
   raws_filterDate(20170901, 20171101, timezone = "America/Los_Angeles") %>%
   raws_extractData(forOpenair = TRUE)
 
-AirSensor::timeseriesTbl_multiPlot(data_2017)
-
+# *openair* timeseries plot
 openair::timePlot(
   data_2017,
   pollutant = c("temperature", "humidity"),
