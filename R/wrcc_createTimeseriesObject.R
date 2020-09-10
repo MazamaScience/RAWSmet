83f7c72232fc7318a60ffe4906c9b077c219d66b
Eli Grosman <eligrosman1@gmail.com>
2020-09-10 00:51:54 -0700

    set standard columns for wrcc timeseries objects

--- R/wrcc_createTimeseriesObject.R
+++ R/wrcc_createTimeseriesObject.R
@@ -88,64 +88,38 @@ wrcc_createTimeseriesObject <- function(
   
   # Define the set of standard columns that will always be returned
   standardColumns <- c(
-    "datetime", "temperature", "humidity",
-    "windSpeed", "windDirection", "maxGustSpeed", "maxGustDirection",
-    "precipitation", "solarRadiation"
+    "datetime", "AvAirTemp", "RelHumidty",
+    "WindSpeed", "WindDirec", "MxGustSpeed", "DirMxGust",
+    "Precip", "SolarRad", "BatteryVoltage", "FuelTemp",
+    "AvFuelMoistr"
   )
   
-  if ( monitorType == "WRCC_TYPE1") {
-    
-    # # A tibble: 6 x 13
-    #   LST_datestamp Precip WindSpeed WindDirec AvAirTemp FuelTemp RelHumidty BatteryVoltage AvFuelMoistr DirMxGust MxGustSpeed SolarRad monitorType
-    #   <chr>         <chr>  <chr>     <chr>     <chr>     <chr>    <chr>      <chr>          <chr>        <chr>     <chr>       <chr>    <chr>      
-    # 1 2008010000    1100   0.447     94        13.89     33.89    NA         13             NA           94        1.341       0        WRCC_TYPE1 
-    # 2 2008010100    1100   0         93        12.78     37.22    NA         13             NA           94        1.341       0        WRCC_TYPE1 
-    
-    data <-
-      tbl %>%
-      dplyr::mutate(
-        "datetime" = paste0("20",.data$LST_datestamp),
-        "temperature" = .data$AvAirTemp,
-        "humidity" = .data$RelHumidty,
-        "windSpeed" = .data$WindSpeed,
-        "windDirection" = .data$WindDirec,
-        "maxGustSpeed" = .data$MxGustSpeed,
-        "maxGustDirection" = .data$DirMxGust,
-        "precipitation" = .data$Precip,
-        "solarRadiation" = .data$SolarRad,
-        "batteryVoltage" = .data$BatteryVoltage,
-        "fuelTemperature" = .data$FuelTemp,
-        "fuelMoisture" = .data$AvFuelMoistr,
-        "monitorType" = .data$monitorType
-      ) %>%
-      dplyr::select(all_of(standardColumns))
-    
-  } else if ( monitorType == "WRCC_TYPE2") {
-    
-    # # A tibble: 6 x 11
-    #   LST_datestamp Precip WindSpeed WindDirec AvAirTemp RelHumidty BatteryVoltage DirMxGust MxGustSpeed SolarRad monitorType
-    #   <chr>         <chr>  <chr>     <chr>     <chr>     <chr>      <chr>          <chr>     <chr>       <chr>    <chr>      
-    # 1 2008010000    171.5  5.812     245       22.22     35         12.9           241       7.6         0        WRCC_TYPE2 
-    # 2 2008010100    171.5  5.812     250       21.67     36         12.9           234       8.494       0        WRCC_TYPE2 
-
-    data <-
-      tbl %>%
-      dplyr::mutate(
-        "datetime" = paste0("20",.data$LST_datestamp),
-        "temperature" = .data$AvAirTemp,
-        "humidity" = .data$RelHumidty,
-        "windSpeed" = .data$WindSpeed,
-        "windDirection" = .data$WindDirec,
-        "maxGustSpeed" = .data$MxGustSpeed,
-        "maxGustDirection" = .data$DirMxGust,
-        "precipitation" = .data$Precip,
-        "solarRadiation" = .data$SolarRad,
-        "batteryVoltage" = .data$BatteryVoltage,
-        "monitorType" = .data$monitorType
-      ) %>%
-      dplyr::select(all_of(standardColumns))
-    
+  # If any of the standard columns don't exist, replace them with NA
+  for ( column in standardColumns) {
+    if ( !column %in% names(tbl) ) {
+      tbl[column] <- as.character(NA)
+    }
   }
+  
+  data <-
+    tbl %>%
+    dplyr::mutate(
+      "datetime" = paste0("20",.data$LST_datestamp),
+      "temperature" = .data$AvAirTemp,
+      "humidity" = .data$RelHumidty,
+      "windSpeed" = .data$WindSpeed,
+      "windDirection" = .data$WindDirec,
+      "maxGustSpeed" = .data$MxGustSpeed,
+      "maxGustDirection" = .data$DirMxGust,
+      "precipitation" = .data$Precip,
+      "solarRadiation" = .data$SolarRad,
+      "batteryVoltage" = .data$BatteryVoltage,
+      "fuelTemperature" = .data$FuelTemp,
+      "fuelMoisture" = .data$AvFuelMoistr,
+      "monitorType" = .data$monitorType
+    ) %>%
+    dplyr::select(all_of(standardColumns))
+    
   
   # * Convert datetime to UTC ----
 
